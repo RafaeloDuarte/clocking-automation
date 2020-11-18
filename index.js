bf80ab763c9677a5043838ad6823906769764aa8
Rafael Duarte <rduarte@sccalpha.local>
2020-11-18 14:20:34 -0300

    second

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const { Keyboard } = require('selenium-webdriver/lib/input');
- 
+const chrome = require('selenium-webdriver/chrome');
+var robot = require("robotjs");
+
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
+
   driver.get("https://rhid.com.br/v2/#/login")
     .then(function() {
       return driver.wait(until.elementLocated(By.name("email")), 20000)
@@ -20,6 +23,19 @@ const { Keyboard } = require('selenium-webdriver/lib/input');
           driver.sleep(500).then(function(){
             return driver.findElement(By.id("m_login_signin_submit")).click()
           })
+
+          driver.sleep(2000).then(function(){
+            return driver.findElement(By.xpath('//*[@id="m_ver_menu"]/ul/li[2]/a/span')).click()
+          })
+
+          driver.sleep(2500).then(function(){
+            return driver.findElement(By.xpath('//*[@id="m_ver_menu"]/ul/li[2]/div/ul/li[2]/a/span')).click()
+          })
+
+          driver.sleep(350000).then(function(){
+            return driver.findElement(By.xpath('//*[@id="id_btnCamPerson"]')).click()
+          })
+
       })
   })
 })();
